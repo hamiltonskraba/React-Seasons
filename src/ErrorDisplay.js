a3c2ac5bd99853ab557284fcb6ac7c24b1e2899a
Hamilton <hamiltonskrabanek@gmail.com>
2018-12-31 12:07:56 -0600

    finish error display page

--- src/ErrorDisplay.js
+++ src/ErrorDisplay.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 const ErrorDisplay = (props) => {
 	return (
-			<h1>Error:{props.errorMessage} </h1>
+		<div className="errDisplay">
+			<i class="stop circle icon icon-left massive"></i>
+				<div>
+					<h1>
+						<p>Error: {props.errorMessage} </p>
+					</h1>
+				</div>
+			<i class="stop circle icon icon-right massive"></i>	
+		</div>	
 	);
 }
 
